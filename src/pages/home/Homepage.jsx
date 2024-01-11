02b82b46a4ba8883bfa66987151b788c07850428
Sargsyan555 <sargsyanromik6@gmail.com>
2024-01-11 19:48:53 +0300

    changes

---
+++ src/pages/home/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Homepage.css'
+export default function Homepage() {
+  return (
+    <div className='homepage'>
+      <a href="computer_architecture">Computer Architecture</a>
+      <a href="c_programming">C Programming</a>
+      <a href="javascript">JavaScript</a>
+      <a href="linux">Linux</a>
+    </div>
+  )
+}
