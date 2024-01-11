8e73f198889c0867da4ba8de61e91893f685161a
Sargsyan555 <sargsyanromik6@gmail.com>
2024-01-11 20:28:01 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import About from './components/About';
 import Homepage from './pages/home/Homepage';
 
